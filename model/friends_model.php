22e3c20b3a8f526828e38f6425e0196bc9d29f31
Mahesh Belnekar <mbelnek@iu.edu>
2019-04-20 17:55:11 -0400

    Implemented getFriends for friend_model

--- model/friends_model.php
+++ model/friends_model.php
@@ -6,16 +6,34 @@ require $_SERVER['DOCUMENT_ROOT']."/planit/dbconnection/dbconnect.php";
  * Get all the friends of this user
  * 
  * @params  username
- * @return  list of objects with friends details
+ * @return  array of objects with friends details(username,fullname)
  */
 function getFriends($username){
 
+    $customerQuery = sendQuery("SELECT friend,friendname FROM friends where username = '$username'");
+    $friendArray = array();
+    // output data of each row
+    while($row = $customerQuery->fetch_assoc()) {
+        // create object (username,fullname)
+        $userProfile = new stdClass();
+        $userProfile->fusername = $row["friend"];
+        $userProfile->fname = $row["friendname"];
+        // add it to our array
+        array_push($friendArray,$userProfile);
+    }
+    
+    // print all our friends
+    // foreach ($friendArray as $friend) {
+    //     echo "friend: " . $friend->fusername. " - friendname: " . $friend->fname. "<br>";
+    // }
 }
 
 /**
  * Add friends in the friend list 
- * @params list of friends to be added, username
+ * @params array of friends to be added, username
  */
-function addFriends($friendList, $username) {
+function addFriends($friendArray, $username) {
 }
+
+// getFriends("vbhor");
 ?>
