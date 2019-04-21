868ed241643b341ea6224f320699cc2e7695969d
Mahesh Belnekar <mbelnek@iu.edu>
2019-04-20 21:40:44 -0400

    added fields to fetch fullname with task

--- model/task_model.php
+++ model/task_model.php
@@ -3,6 +3,20 @@
 
 // Will return details for the given task
 function getTaskDetails($taskId){
+    // We also get a dictionary of username => fullname, this will be useful to us when
+    // we have to get the fullname using the username
+    $nameQuery = sendQuery("SELECT * from person");
+    $nameArray = array();
+    while($row = $nameQuery->fetch_assoc()) {
+        // add it to our array
+        $nameArray[$row["username"]] = $row["fullname"];
+    }
+
+    // print all users
+    // foreach ($nameArray as $name) {
+    //     echo "username: " . $nameArray['vbhor'] . "<br>";
+    // }
+
     $taskQuery = sendQuery("SELECT * FROM planit.task where taskid = '$taskId'");
     $taskArray = array();
     // output data of each row
@@ -14,24 +28,39 @@ function getTaskDetails($taskId){
     $taskObj->deadline = $row["deadline"];
     $taskObj->status = $row["status"];
     $taskObj->assignedTo = $row["assignedTo"];
+    $taskObj->assignedToName = $nameArray[$taskObj->assignedTo];
     $taskObj->eventid = $row["eventid"];
     $taskObj->assignedBy = $row["assignedBy"];
+    $taskObj->assignedByName = $nameArray[$taskObj->assignedBy];
     $taskObj->modifiedBy = $row["modifiedBy"];
+    $taskObj->modifiedByName = $nameArray[$taskObj->modifiedBy];
     
     // print task details
-    // echo "task: " . $taskObj->title. " - edate: " . $taskObj->assignedTo. "<br>";
+    // echo "task: " . $taskObj->assignedToName. " - edate: " . $taskObj->assignedByName. "<br>";
 
     return $taskObj;
 }
 
 // Will return all the tasks assigned to this user
 function getTasksByUsername($username){
+    // We also get a dictionary of username => fullname, this will be useful to us when
+    // we have to get the fullname using the username
+    $nameQuery = sendQuery("SELECT * from person");
+    $nameArray = array();
+    while($row = $nameQuery->fetch_assoc()) {
+        // add it to our array
+        $nameArray[$row["username"]] = $row["fullname"];
+    }
+
+    // print all users
+    // foreach ($nameArray as $name) {
+    //     echo "username: " . $nameArray['vbhor'] . "<br>";
+    // }
+
     $taskQuery = sendQuery("SELECT * FROM planit.task where assignedTo = '$username'");
     $taskArray = array();
     // output data of each row
     while($row = $taskQuery->fetch_assoc()) {
-        // we need to get 
-
         // create object (username,fullname)
         $taskObj = new stdClass();
         $taskObj->taskid = $row["taskid"];
@@ -39,21 +68,22 @@ function getTasksByUsername($username){
         $taskObj->deadline = $row["deadline"];
         $taskObj->status = $row["status"];
         $taskObj->assignedTo = $row["assignedTo"];
+        $taskObj->assignedToName = $nameArray[$taskObj->assignedTo];
         $taskObj->eventid = $row["eventid"];
         $taskObj->assignedBy = $row["assignedBy"];
+        $taskObj->assignedByName = $nameArray[$taskObj->assignedBy];
         $taskObj->modifiedBy = $row["modifiedBy"];
+        $taskObj->modifiedByName = $nameArray[$taskObj->modifiedBy];
 
         // add it to our array
         array_push($taskArray,$taskObj);
     }
     
-    // print all our friends
+    // print all tasks
     // foreach ($taskArray as $task) {
-    //     echo "task: " . $task->title. " - edate: " . $task->assignedTo. "<br>";
+    //      echo "assignedToName: " . $task->assignedToName. " - assignedByName: " . $task->assignedByName. "<br>";
     // }
-
     return $taskArray;
-
 }
 
 // Will return all the tasks in this event
