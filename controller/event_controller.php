635ab27e9fdb19d5bad29e1419178a2e6273548b
Mahesh Belnekar <mbelnek@iu.edu>
2019-04-21 13:26:18 -0400

    Updated task and event controller not to include dbconnection

--- controller/event_controller.php
+++ controller/event_controller.php
@@ -1,26 +1,4 @@
 <?php
-require $_SERVER['DOCUMENT_ROOT']."/planit/dbconnection/dbconnect.php";;
+// require $_SERVER['DOCUMENT_ROOT']."/planit/dbconnection/dbconnect.php";;
 
-/**
- * Get all events for the given user
- * @params username
- * @return list of event objects, each object containing event details
- */
-function getEvents($username){
-}
-
-/**
- * Add event
- * @params event object, should contain all data
- */
-function addEvent($event) {
-}
-
-/**
- * Get all members in the event
- * @params eventId
- * @return list of member objects, each object containing member details
- */
-function getEventMembers($eventId){
-}
 ?>
