e6cc627f7c7e25c10847e177827b69e6e9cbef50
Mahesh Belnekar <mbelnek@iu.edu>
2019-04-20 22:04:53 -0400

    Added extra fileds in task_model functions

--- model/task_model.php
+++ model/task_model.php
@@ -34,6 +34,7 @@ function getTaskDetails($taskId){
     $taskObj->assignedByName = $nameArray[$taskObj->assignedBy];
     $taskObj->modifiedBy = $row["modifiedBy"];
     $taskObj->modifiedByName = $nameArray[$taskObj->modifiedBy];
+    $taskObj->description = $row["description"];
     
     // print task details
     // echo "task: " . $taskObj->assignedToName. " - edate: " . $taskObj->assignedByName. "<br>";
@@ -74,6 +75,7 @@ function getTasksByUsername($username){
         $taskObj->assignedByName = $nameArray[$taskObj->assignedBy];
         $taskObj->modifiedBy = $row["modifiedBy"];
         $taskObj->modifiedByName = $nameArray[$taskObj->modifiedBy];
+        $taskObj->description = $row["description"];
 
         // add it to our array
         array_push($taskArray,$taskObj);
@@ -88,6 +90,20 @@ function getTasksByUsername($username){
 
 // Will return all the tasks in this event
 function getTasksByEvent($eventid){
+    // We also get a dictionary of username => fullname, this will be useful to us when
+    // we have to get the fullname using the username
+    $nameQuery = sendQuery("SELECT * from person");
+    $nameArray = array();
+    while($row = $nameQuery->fetch_assoc()) {
+        // add it to our array
+        $nameArray[$row["username"]] = $row["fullname"];
+    }
+
+    // print all users
+    // foreach ($nameArray as $name) {
+    //     echo "username: " . $nameArray['vbhor'] . "<br>";
+    // }
+
     $taskQuery = sendQuery("SELECT * FROM planit.task where eventid = '$eventid'");
     echo "SELECT * FROM planit.task where eventid = '$eventid'";
     $taskArray = array();
@@ -100,9 +116,13 @@ function getTasksByEvent($eventid){
         $taskObj->deadline = $row["deadline"];
         $taskObj->status = $row["status"];
         $taskObj->assignedTo = $row["assignedTo"];
+        $taskObj->assignedToName = $nameArray[$taskObj->assignedTo];
         $taskObj->eventid = $row["eventid"];
         $taskObj->assignedBy = $row["assignedBy"];
+        $taskObj->assignedByName = $nameArray[$taskObj->assignedBy];
         $taskObj->modifiedBy = $row["modifiedBy"];
+        $taskObj->modifiedByName = $nameArray[$taskObj->modifiedBy];
+        $taskObj->description = $row["description"];
 
         // add it to our array
         array_push($taskArray,$taskObj);
