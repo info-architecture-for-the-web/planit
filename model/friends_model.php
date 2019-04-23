64f71f0a362252d7aa6847fea890c4d1efedb32d
Mahesh Belnekar <mbelnek@iu.edu>
2019-04-23 02:07:39 -0400

    Implemented changes to add new friends, controller and model

--- model/friends_model.php
+++ model/friends_model.php
@@ -59,9 +59,44 @@ function getAllUsers()
 }
 
 /**
- * Add friends in the friend list 
+ * Add friendship between user1 and user2
  * @params array of friends to be added, username
  */
-function addFriends($friendArray, $username) {
+function addFriendship($user1, $user2) {
+
+    // We get a dictionary of username => fullname, this will be useful to us when
+    // we have to get the fullname using the username
+    $nameQuery = sendQuery("SELECT * from person");
+    $nameArray = array();
+    while($row = $nameQuery->fetch_assoc()) {
+        // add it to our array
+        $nameArray[$row["username"]] = $row["fullname"];
+    }
+
+    // We need to add two entries
+    // 1. user1 friends with user2(user2's username, user2's fullname)
+    // 2. user2 friends with user1(user1's username, user1's fullname)
+    // We add this redundancy to improve fetching data at runtime, as
+    // #read_requests >> #write_requests
+    
+    // First insert query:
+    $insertQuery = sendQuery("INSERT INTO `planit`.`friends`
+    (`username`,
+    `friend`,
+    `friendname`)
+    VALUES
+    ('$user1',
+    '$user2',
+    '$nameArray[$user2]')");
+
+    // Second insert query:
+    $insertQuery = sendQuery("INSERT INTO `planit`.`friends`
+    (`username`,
+    `friend`,
+    `friendname`)
+    VALUES
+    ('$user2',
+    '$user1',
+   ' $nameArray[$user1]')");
 }
 ?>
